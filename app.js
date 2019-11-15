e7be611589af6a1a3fa39c7116a31cd7bfe0a613
g-craft <48797306+g-craft@users.noreply.github.com>
2019-11-15 13:47:04 +0100

    Add files via upload
    Add mongoDB

--- app.js
+++ app.js
@@ -2,58 +2,146 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js")
+const _ = require("lodash");
+const mongoose = require("mongoose");
 const app = express();
+
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
-app.set("view engine", "ejs");
 
-let items = ["Work"];
-let workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true
+});
+
+const itemsSchema = new mongoose.Schema({
+  name: String
+});
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "New ToDo list"
+});
+const item2 = new Item({
+  name: "Hit button + to ad a new item"
+});
+const item3 = new Item({
+  name: "<-- Hit this to delete an item"
+});
+const defaultItems = [item1, item2, item3];
+
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listSchema);
 
 app.get("/", function(req, res) {
 
-  let day = date.getDate();
-
-  res.render("list", {
-    listTitle: day,
-    newListItems: items
+  Item.find({}, function(err, foundItems) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundItems.length === 0) {
+        Item.insertMany(defaultItems, function(err) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Successfully saved default items");
+          }
+        });
+        res.redirect("/");
+      } else {
+        res.render("list", {
+          listTitle: "Today",
+          newListItems: foundItems
+        });
+      }
+    }
   });
 });
 
-app.listen(3000, function() {
-  console.log("app is running on port 3000");
+app.get("/:customListName", function(req, res) {
+  const customListName = _.capitalize(req.params.customListName);
+
+  List.findOne({
+    name: customListName
+  }, function(err, foundList) {
+    if (!err) {
+      if (!foundList) {
+        const list = new List({
+          name: customListName,
+          items: defaultItems
+        });
+        list.save();
+        res.redirect("/" + customListName);
+      } else {
+        res.render("list", {
+          listTitle: foundList.name,
+          newListItems: foundList.items
+        });
+      }
+    }
+  });
 });
+
+
 
 app.post("/", function(req, res) {
 
-  let item = req.body.newItem;
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
-
-});
-
-app.get("/work", function(req, res) {
-  res.render("list", {
-    listTitle: "Work List",
-    newListItems: workItems
+  const newItemName = req.body.newItem;
+  const listName = req.body.list;
+  const newItem = new Item({
+    name: newItemName
   });
+  if (listName === "Today") {
+    newItem.save();
+    res.redirect("/");
+  } else {
+    List.findOne({
+      name: listName
+    }, function(err, foundList) {
+      foundList.items.push(newItem);
+      foundList.save();
+      res.redirect("/" + listName);
+    });
+  }
 });
 
-app.post("/work", function(req, res) {
-  let item = req.body.newItem;
-  workItems.push(item);
-  res.redirect("/work");
+app.post("/delete", function(req, res) {
+  const idDelete = req.body.checkbox;
+  const listName = req.body.listName;
+  console.log(idDelete);
+  if (listName === "Today") {
+    Item.findByIdAndRemove(idDelete, function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("removed!" + idDelete);
+        res.redirect("/");
+      }
+    });
+  } else {
+    List.findOneAndUpdate({
+      name: listName
+    }, {
+      $pull: {
+        items: {
+          _id: idDelete
+        }
+      }
+    }, function(err, foundList) {
+      if (!err) {
+        res.redirect("/" + listName);
+      }
+    });
+  }
 });
 
-app.get("/about", function(req, res) {
-  res.render("about");
+app.listen(3000, function() {
+  console.log("Server has started successfully");
 });
